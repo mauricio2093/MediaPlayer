3a25139904da76679c91a6eaca207216fb933144
mauricio2093 <mauricio2093@gmail.com>
2022-05-17 23:15:37 -0500

    actualizando js y visibilidad pause

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -3,13 +3,16 @@ class AutoPause{
         this.video = video;
         this.threshold = 0.35;
         this.pausarVideo = this.pausarVideo.bind(this)
+        this.pausarVideoVisibilityChange = this.pausarVideoVisibilityChange.bind(this)
     }
     run(){
         const observador = new IntersectionObserver(this.pausarVideo, {
             root : null,
             threshold: this.threshold,
         });
-        observador.observe(this.video)
+        observador.observe(this.video);
+
+        document.addEventListener("visibilitychange", this.pausarVideoVisibilityChange)
     }
     pausarVideo(entries){
         const entry = entries[0];
@@ -21,8 +24,11 @@ class AutoPause{
         }else{
             this.video.pause();
         };
-        
-        
+    }
+
+    pausarVideoVisibilityChange(){
+        const isVisible =  document.visibilityState === "visible";
+        isVisible ? this.video.play() : this.video.pause();
     }
 }
 
